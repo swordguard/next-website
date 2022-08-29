e876a2f5cb74859470a09e7bc1a6a45856f9ee24
Jian <jay.sun71503@gmail.com>
2022-08-29 19:21:50 +1000

    Updates

--- static/ost482Y0IEIaIA-DO3BHQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/769-c8de1221afd053bd.js","static/css/ae0e3e027412e072.css","static/chunks/pages/index-2c1fb9b70b01ed8c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
